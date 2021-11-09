cd70683e4c3e901b87517408fa8b7ce2ddc7ba65
Alioune1206 <25acisse@overlake.org>
2021-11-09 09:06:08 -0800

    git

--- Lab 03 - Draw Using Functions/Practice_animation.py
+++ Lab 03 - Draw Using Functions/Practice_animation.py
@@ -4,35 +4,40 @@
 # Set the dimensions (width and height)
 import arcade
 
-arcade.open_window(750, 750, "Space Needle")
 
-# Set the background color
-arcade.set_background_color(arcade.csscolor.SKY_BLUE)
 
-# Get ready to draw
-arcade.start_render()
+def space_needle(x,y):
+    arcade.draw_ellipse_filled(x, y, 50, 300, arcade.csscolor.CADET_BLUE)
+    arcade.draw_rectangle_filled(x, y - 325, 100, 600, arcade.csscolor.WHITE_SMOKE)
+    arcade.draw_ellipse_filled(x, y - 25, 400, 150, arcade.csscolor.WHITE_SMOKE)
+    arcade.draw_ellipse_filled(x, y - 25, 600, 25, arcade.csscolor.CADET_BLUE)
+    arcade.draw_ellipse_filled(x - 60, y - 370, 79, 550, arcade.csscolor.SKY_BLUE)
+    arcade.draw_ellipse_filled(x + 50, y - 370, 79, 550, arcade.csscolor.SKY_BLUE)
 
-# Draw an Ellipse
-arcade.draw_ellipse_filled(300, 575, 50, 300, arcade.csscolor.CADET_BLUE)
+def bird(x,y):
+    arcade.draw_ellipse_filled(x, y, 160, 40, arcade.csscolor.FLORAL_WHITE)
+    arcade.draw_circle_filled(x - 50, y + 10, 10, arcade.csscolor.BLACK)
+    arcade.draw_ellipse_filled(x, 650, y - 650, 100, arcade.csscolor.FLORAL_WHITE)
+    arcade.draw_ellipse_filled(x - 80, y, 25, 25, arcade.csscolor.YELLOW)
 
-# Tree trunk
-arcade.draw_rectangle_filled(300, 250, 100, 600, arcade.csscolor.WHITE_SMOKE)
+def sun(x,y):
+    arcade.draw_circle_filled(x, y, 100, arcade.color.YELLOW)
 
-# Tree Top
-arcade.draw_ellipse_filled(300, 550, 400, 150, arcade.csscolor.WHITE_SMOKE)
-arcade.draw_ellipse_filled(300, 550, 600, 25, arcade.csscolor.CADET_BLUE)
-arcade.draw_ellipse_filled(240, 205, 79, 550, arcade.csscolor.SKY_BLUE)
-arcade.draw_ellipse_filled(350, 205, 79, 550, arcade.csscolor.SKY_BLUE)
+def on_draw(delta_time):
+    """draw everything"""
+    arcade.start_render()
+    space_needle(400, 600)
+    space_needle(200, 100)
+    bird(bird_x, 500)
+    bird(bird_x + 50, 700)
+    bird_x += 1
+bird_x = 450
 
-# Draw a Plane
-arcade.draw_ellipse_filled(450, 700, 160, 40, arcade.csscolor.FLORAL_WHITE)
-arcade.draw_circle_filled(400, 710, 10, arcade.csscolor.BLACK)
-arcade.draw_ellipse_filled(450, 650, 50, 100, arcade.csscolor.FLORAL_WHITE)
-arcade.draw_ellipse_filled(370, 700, 25, 25, arcade.csscolor.YELLOW)
+def main():
+    arcade.open_window(750, 750, "Space Needle")
+    arcade.set_background_color(arcade.csscolor.SKY_BLUE)
+    arcade.schedule(on_draw, 1/60)
 
-# Draw a sun
-arcade.draw_circle_filled(700, 700, 100, arcade.color.YELLOW)
+    arcade.run()
 
-arcade.finish_render()
-
-arcade.run()
+main()
